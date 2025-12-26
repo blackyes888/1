729b002e60bd5d74f690e0dc233653b578c2ab39
blackyes888 <blackyes888@gmail.com>
2025-12-26 07:11:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -27,24 +27,22 @@ if (preg_match($botchar, $res)) {
 /**
  * @defgroup pages_index Index Pages
  */
- 
+
 /**
  * @file pages/index/index.php
  *
- * Copyright (c) 2014-2018 Simon Fraser University
- * Copyright (c) 2003-2018 John Willinsky
- * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ * Copyright (c) 2014-2021 Simon Fraser University
+ * Copyright (c) 2003-2021 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @ingroup pages_index
- * @brief Handle site index requests. 
+ *
+ * @brief Handle site index requests.
  *
  */
 
 switch ($op) {
-	case 'index':
-		define('HANDLER_CLASS', 'IndexHandler');
-		import('pages.index.IndexHandler');
-		break;
+    case 'index':
+        define('HANDLER_CLASS', 'APP\pages\index\IndexHandler');
+        break;
 }
-
-?>
